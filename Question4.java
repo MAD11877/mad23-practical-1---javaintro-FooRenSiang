afa1f06ac54b3f55a849aa907b2c4e28fb47634d
FooRenSiang <130948785+FooRenSiang@users.noreply.github.com>
2023-04-17 17:02:43 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -4,7 +4,7 @@ public class Question4
 {
   public static void main(String[] args)
   {
-    System.out.print("Enter integer:");
+    //System.out.print("Enter integer:");
     Scanner in = new Scanner(System.in);
     int loop = in.nextInt();
     for (int i = loop; i > 0; i--){
