c5f81c91b13056ebe5d17a9ad827ca9e636a16c8
FooRenSiang <130948785+FooRenSiang@users.noreply.github.com>
2023-04-17 10:55:26 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -1,12 +1,5 @@
-public class Question1
-{
-  public static void main(String[] args)
-  {
-    /**
-     * Print "Hello World!"
-     * 
-     * Hint: System.out.println(...)
-     */
-    System.out.println("Hello World!");
-  }
+class Main {  
+  public static void main(String args[]) { 
+    System.out.println("Hello World!"); 
+  } 
 }
